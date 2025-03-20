2b3abeb4db009f4b2327750fa4eb905f0e8d012d
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-03-20 20:15:10 +0100

    Bonus added. Exercise completed

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <title>Primi passi con Laravel</title>
+    <title>Contatti</title>
 </head>
 
 <body>
@@ -28,7 +28,7 @@
         </nav>
     </header>
 
-    <h1>Hello World!</h1>
+    <h1>Sei nella pagina:</h1>
 
     <p>{{$message}}</p>
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['message' => "Benvenuto su Laravel! Questo è il tuo primo passaggio di dati dinamici alla view!"]);
+    return view('home', ['message' => "Sei nella Homepage"]);
+})->name('home');
+
+Route::get('/service-and-products', function () {
+    return view('service-products', ['message' => "Sei nella pagina Servizi e Prodotti"]);
+})->name('service-products');
+
+Route::get('/about-us', function () {
+    return view('about-us', ["message" => "Sei nella pagina Chi siamo"]);
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contacts', ['message' => 'Sei nella pagina Contatti']);
+})->name('contacts');
+
+// Rotta di fallback (acchiappatutto) che intercetta tutte le richieste che non sono state matchate
+Route::fallback(function(){
+    return "<h1>Errore: 404 - Pagina non trovata</h1>";
 });
+
+?>
